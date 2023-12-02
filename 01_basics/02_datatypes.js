808e1a3f06731c2fc64bd0820178bd9ea6865c82
SAPTAPARNA99 <137778802+SAPTAPARNA99@users.noreply.github.com>
2023-12-02 08:16:25 +0000

    data type

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,21 @@
-alart (3+3)
+// alart (3+3) // we are using nodejs ,not browser
+console.log(3+3);
+
+
+let name ="sapta"
+let age = 24
+let isLoggedIn =false
+
+//number => 2 to power 53
+//bigint
+// string => ""
+//boolean =>true/false
+//null => standalone value
+//undefine =>
+//symbol => unique
+
+//object
+
+console.log(typeof age );
+console.log(typeof undefined );//undefined
+console.log(typeof null );//object
