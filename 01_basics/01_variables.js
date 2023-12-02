264f3986ac0f8864e602133283122d73403619ee
SAPTAPARNA99 <137778802+SAPTAPARNA99@users.noreply.github.com>
2023-12-02 07:48:43 +0000

    variable  declair

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail ="sapta@google.com"
+var accountPassword="1234"
+accountCity ="jaipur"
+// accountId =12
+/*
+PREFER NOT TO USE VAR BECAUSE OF ISUUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
+let accountState
+accountEmail ="saptaparna@google.com"
+console.log(accountEmail);
+console.table([accountId , accountEmail ,accountPassword ,accountCity, accountState])
