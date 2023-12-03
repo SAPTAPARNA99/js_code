61a79aa73e8bd591423367f90c264983ff2e7db7
SAPTAPARNA99 <137778802+SAPTAPARNA99@users.noreply.github.com>
2023-12-03 15:14:44 +0000

    string

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,7 +4,7 @@ console.log(3+3);
 
 let name ="sapta"
 let age = 24
-let isLoggedIn =false
+//let isLoggedIn =false
 
 //number => 2 to power 53
 //bigint
@@ -19,3 +19,29 @@ let isLoggedIn =false
 console.log(typeof age );
 console.log(typeof undefined );//undefined
 console.log(typeof null );//object
+
+
+/////
+
+
+let score ="33"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=> 33
+// "33abc"=> NAN
+//true =>1; false =>0
+
+let isLoggedIn =""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true
+//0=>false
+//"sapta"=>true
+//""=>false
